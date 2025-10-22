ee1a332740b23e8e4b89fcc28cf2bff46579426c
IrvingYael05 <irving.vazser@gmail.com>
2025-10-22 00:24:45 -0600

    Add new icons and update web manifest for mobile application
    - Added new PNG icons in various sizes (72x72, 96x96) for the application.
    - Updated the web manifest to include the new icons with appropriate sizes and types.
    - Ensured all icons are marked as "maskable" for better display on different devices.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +15,9 @@ export const appConfig: ApplicationConfig = {
         preset:Aura
       }
     }),
-    provideAnimations(),
+    provideAnimations(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 };
